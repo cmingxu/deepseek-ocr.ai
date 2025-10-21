971402c45bd3481603bfcccb2fac2bab9f477913
kevin.xu <cming.xu@gmail.com>
2025-10-21 08:40:37 +0800

    enable anlytic
    Signed-off-by: kevin.xu <cming.xu@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -118,6 +119,7 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLdOrg) }}
         />
       </body>
+      <Analytics />
     </html>
   );
 }
